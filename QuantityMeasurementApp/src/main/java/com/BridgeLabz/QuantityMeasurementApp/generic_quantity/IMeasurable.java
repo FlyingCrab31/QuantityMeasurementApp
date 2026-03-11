fb9a0a91aceb9914cd74e105d6a67da17bcbafa9
FlyingCrab31 <rachitp25@gmail.com>
2026-03-11 11:35:40 +0530

    [Rishabh Pandey] Add .Added Temperature Measurement with selective Airthmetic Support and Measurable Refactoring

--- QuantityMeasurementApp/src/main/java/com/BridgeLabz/QuantityMeasurementApp/generic_quantity/IMeasurable.java
+++ QuantityMeasurementApp/src/main/java/com/BridgeLabz/QuantityMeasurementApp/generic_quantity/IMeasurable.java
@@ -3,11 +3,12 @@ package com.bridgelabz.quantityMeasurementApp.generic_quantity;
 public interface IMeasurable {
     double getConversionFactor();
     default double convertToBaseUnit(double value) {
+
         return value * getConversionFactor();
     }
     default double convertFromBaseUnit(double baseValue) {
         return baseValue / getConversionFactor();
     }
-
     String getUnitName();
+    default boolean supportsArithmetic() {return true;}
 }
---
+++ QuantityMeasurementApp/src/main/java/com/BridgeLabz/QuantityMeasurementApp/generic_quantity/SupportsArithmetic.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.quantityMeasurementApp.generic_quantity;
+@FunctionalInterface
+public interface SupportsArithmetic {
+    boolean isSupported();
+}
