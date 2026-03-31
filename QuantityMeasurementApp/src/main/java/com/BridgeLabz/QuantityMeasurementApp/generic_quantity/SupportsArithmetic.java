a2c2ca6a584c5c679ed9e863a48888647a9cab66
FlyingCrab31 <rachitp25@gmail.com>
2026-03-31 11:17:58 +0530

    [Rishabh Pandey] Add .Added Spring Integration

--- QuantityMeasurementApp/src/main/java/com/BridgeLabz/QuantityMeasurementApp/generic_quantity/SupportsArithmetic.java
+++
@@ -1,5 +0,0 @@
-package com.bridgelabz.quantityMeasurementApp.generic_quantity;
-@FunctionalInterface
-public interface SupportsArithmetic {
-    boolean isSupported();
-}
--- QuantityMeasurementApp/src/main/java/com/BridgeLabz/QuantityMeasurementApp/repository/IQuantityMeasurementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.quantityMeasurementApp.repository;
-import com.bridgelabz.quantityMeasurementApp.entity.QuantityMeasurementEntity;
-import java.util.List;
-public interface IQuantityMeasurementRepository {
-    void save(QuantityMeasurementEntity entity);
-
-    List<QuantityMeasurementEntity> findAll();
-}
